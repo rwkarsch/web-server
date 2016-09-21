4a39bd360d82b8f922e707e2bfe5493472012f60
Robert Karsch <robertkarsch@gmail.com>
2016-09-21 05:46:48 -0400

    Update language

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var middleware = {
 		next();
 	},
 	logger: function (req, res, next){
-		
 		newDate = new Date().toString();
 		console.log('Request: ' + newDate + ' - ' + req.method + ' ' + req.originalUrl);
 		next();
@@ -18,7 +17,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-	res.send('About Us!');
+	res.send('About Us');
 })
 
 app.use(express.static(__dirname + '/public'));
