b36deb826b2860f20f24087ff920f3eb2b538f14
Robert Karsch <robertkarsch@gmail.com>
2016-09-21 05:45:14 -0400

    Initialize repo

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var PORT = 3300;
+
+var middleware = {
+	requireAuthentication: function (req, res, next){
+		console.log('private log hit!');
+		next();
+	},
+	logger: function (req, res, next){
+		
+		newDate = new Date().toString();
+		console.log('Request: ' + newDate + ' - ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us!');
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT);
+});
